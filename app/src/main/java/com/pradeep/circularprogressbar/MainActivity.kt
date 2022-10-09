607f588f1a405f0b40c09a68670031c43a1569ea
Pradeep <iampradeephr@gmail.com>
2022-10-09 10:23:32 +0530

    .apk file build

--- app/src/main/java/com/pradeep/circularprogressbar/MainActivity.kt
+++ app/src/main/java/com/pradeep/circularprogressbar/MainActivity.kt
@@ -1,31 +1,47 @@
 package com.pradeep.circularprogressbar
 
+import android.content.Context
 import android.os.Bundle
+import android.view.animation.Transformation
+import android.widget.ImageView.ScaleType
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.Coil
+import coil.ImageLoader
+import coil.compose.*
+import coil.request.ImageRequest
+import coil.size.Scale
+import coil.transform.CircleCropTransformation
 import com.pradeep.circularprogressbar.ui.theme.CircularProgressBarTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,24 +49,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CircularProgressBarTheme {
-                
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.Center
-                ){
-                    CircularProgressBar(
-                        percentage = 0.8f,
-                        number = 100,
-                        radius = 100.dp,
-                        fontSize = MaterialTheme.typography.h3.fontSize,
-                        strokeWidth = 20.dp
-                    )
+                Box(modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center){
+
                 }
+
             }
         }
     }
 }
+
+
+
+
 
 @Composable
 fun CircularProgressBar(
